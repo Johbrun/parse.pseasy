b8b76aabf5db338bc15fcf3efce514e5d38b9c20
Johan <johan@lonestone.studio>
2021-03-14 00:14:08 +0100

    :tada: Docs + 10000 lignes parsed from 2014

--- README.md
+++ README.md
@@ -7,4 +7,29 @@ pandoc -f docx -t gfm -s 03-PSE2019.docx -o 03-PSE2019.md --reference-links --at
 
 pandoc -f docx -t markdown_mmd -s 03-PSE2019.docx -o ..\md\2019-raw-pandoc.md --reference-links --atx-headers --no-highlight --wrap=auto
 
-pandoc -f docx -t gfm -s 03-PSE2019.docx -o ..\md\2019-raw-pandoc.md --reference-links --atx-headers --no-highlight --wrap=auto
+pandoc -f docx -t gfm -s ./docx/2014.docx -o ./md/2014/\_global.md --reference-links --atx-headers --no-highlight --wrap=auto
+
+\*\* supprimer à la main les parties inutiles
+
+Traitement à la main
+
+- Supprimer les '> '
+- ! Nombre de dièses des titres
+- Modifier à la main les dates dans cartouche pour MoisEnTouteLettres AnnéeEnNombre
+- Ajouter blockquote dans cartouche (pour Reférence : et Version : )
+
+```
+<td>Référence :</td>
+
+# devient
+<td><blockquote>
+<p>Référence :</p>
+</blockquote></td>
+
+```
+
+- Supprimer les break-line "-­‐ "
+
+A checker
+
+- quand une pharse commence par une minuscule, précédé par une ligne vide
--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,142 @@
 import { fromDateFormated } from "./helpers";
 import { SheetCreation } from "./types";
 
-import * as fs from 'fs';
+import * as fs from "fs";
 
-console.log('*** Launch markdown analysis... ***');
+console.log("*** Launch markdown analysis... ***");
 
 // 3 modes : PARSE (arg) / ANALYSIS (always) / UPDATE (arg)
-const PARSE_MODE = false;
+const PARSE_MODE = true;
 const UPDATE_MODE = false;
 const startDate = new Date();
+const YEAR = 2014;
 let nbErr = 0;
 let nbErrSheet = 0;
 
-let sheets: any[] = []
+let sheets: any[] = [];
 let data: SheetCreation;
 
 if (PARSE_MODE) {
+  const mdBuffer = fs.readFileSync("./md/" + YEAR + "/_global_fixed.md").toString() as string;
 
-    const mdBuffer = fs.readFileSync('./docs/md/2019-raw-pandoc.md').toString() as string;
+  // Step 1 : Split all sheets
+  sheets = mdBuffer.split("Référence :");
+  sheets.splice(0, 1); // remove first, it's not a sheet
+  sheets.pop(); // remove last, not a sheet
 
-    // Step 1 : Split all sheets
-    sheets = mdBuffer.split('Référence :')
-    sheets.splice(0, 1) // remove first, it's not a sheet
-    sheets.pop(); // remove last, not a sheet
+  for (let i = 0; i < sheets.length; i++) {
+    let sheet = sheets[i];
 
-    for (let i = 0; i < sheets.length; i++) {
-        let sheet = sheets[i];
+    // Step 4 : Remove / reformat / clean
+    // sheet = sheet.replace(/> /g, "");
+    sheet = sheet.replace(/(^|\s)o\s/g, "\t- "); // replace puces errors
 
-        // Step 4 : Remove / reformat / clean
-        sheet = sheet.replace(/> /g, '');
-        sheet = sheet.replace(/(^|\s)o\s/g, '\t- '); // replace puces errors 
+    // Step 5 : extract and create sheet
+    const a = [...sheet.matchAll(/<p>(.*?)<\/p>/g)];
 
-        // Step 5 : extract and create sheet
-        const a = [...sheet.matchAll(/<p>(.*?)<\/p>/g)];
+    console.log("i", i);
+    data = {
+      reference: a[0][1].replace(/ /g, ""),
+      version: a[2][1],
+      updatedDate: fromDateFormated(a[4][1]),
+      title: "",
+      content: "",
+    };
 
-        data = {
-            reference: a[0][1].replace(/ /g, ''),
-            version: a[2][1],
-            updatedDate: fromDateFormated(a[4][1]),
-            title: '',
-            content: '',
-        }
+    // Step 4.1 Remove tables
+    sheet = sheet.replace(/<\/p>((.|\s\n)*?)<\/table>/, "");
+    // sheet = sheet.replace(/<table>((.|\s\n)*?)<p>/, '');
+    sheet = sheet.replace(/<table><tbody><tr class="odd"><td><blockquote><p>/, "");
+    sheet = sheet.replace(/\`\`\`(.*)\`\`\`/g, "");
+    // sheet = sheet.replace(/```{=html}\s\n<!-- -->\s\n```/g, '')
 
-        // Step 4.1 Remove tables
-        sheet = sheet.replace(/<\/p>((.|\s\n)*?)<\/table>/, '');
-        // sheet = sheet.replace(/<table>((.|\s\n)*?)<p>/, '');
-        sheet = sheet.replace(/<table><tbody><tr class="odd"><td><blockquote><p>/, '');
-        sheet = sheet.replace(/\`\`\`(.*)\`\`\`/g, '')
-        // sheet = sheet.replace(/```{=html}\s\n<!-- -->\s\n```/g, '')
+    const path = `./md/${YEAR}/${data.reference}.md`;
 
-        const path = `./docs/sheets/${data.reference}.md`;
-
-        // End : write file to test
-        if (PARSE_MODE) {
-            sheet = JSON.stringify(data) + '\n' + sheet;
-            fs.writeFileSync(path, sheet/*, (e) => e ? console.error(e.toString()) : undefined*/)
-        }
+    // End : write file to test
+    if (PARSE_MODE) {
+      sheet = JSON.stringify(data) + "\n" + sheet;
+      fs.writeFileSync(path, sheet /*, (e) => e ? console.error(e.toString()) : undefined*/);
     }
+  }
 }
-
-
-const sheetsPath = fs.readdirSync('./docs/sheets/');
+/*
+const sheetsPath = fs.readdirSync("./md/" + YEAR);
 // const sheetsPath = ['AC01G01.md']; // tests
 
 console.log(`[Step 1] - Sheet : ${sheets.length} sheets founds`);
 
 for (let j = 0; j < sheetsPath.length; j++) {
-    const sheet = fs.readFileSync(`./docs/sheets/${sheetsPath[j]}`).toString() as string;
-    data = JSON.parse(sheet.match(/({.*})/)![0]);
+  const sheet = fs.readFileSync(`./md/${YEAR}/${sheetsPath[j]}`).toString() as string;
+  data = JSON.parse(sheet.match(/({.*})/)![0]);
 
-    // now loop on each sheets
-    let i = 0;
-    let error = false;
+  // now loop on each sheets
+  let i = 0;
+  let error = false;
 
-    // Step 2 : Detect errors which needed human correction
+  // Step 2 : Detect errors which needed human correction
 
-    // 2. double title
-    const doubleTitle = sheet.match(/## [A-Z](.*)[a-z]\s[A-Z][a-z](.*)/g);
-    if (doubleTitle) {
-        console.warn("[" + data.reference + "][CHECK] DOUBLE_TITLE      : " + doubleTitle[0])
-        error = true;;
-    }
+  // 2. double title
+  const doubleTitle = sheet.match(/## [A-Z](.*)[a-z]\s[A-Z][a-z](.*)/g);
+  if (doubleTitle) {
+    console.warn("[" + data.reference + "][CHECK] DOUBLE_TITLE      : " + doubleTitle[0]);
+    error = true;
+  }
 
-    // 3. too long titles
-    const toLongTitle = sheet.match(/<\/table>\s\n\s\n[A-Z](.*)/g);
-    if (toLongTitle) {
-        console.warn("[" + data.reference + "][CHECK] INCORRECT_TITLE    : " + toLongTitle[0].substring(0, 100).replace(/\s\n/g, ' '))
-        error = true;
-        nbErr++;
-    }
+  // 3. too long titles
+  const toLongTitle = sheet.match(/<\/table>\s\n\s\n[A-Z](.*)/g);
+  if (toLongTitle) {
+    console.warn(
+      "[" + data.reference + "][CHECK] INCORRECT_TITLE    : " + toLongTitle[0].substring(0, 100).replace(/\s\n/g, " ")
+    );
+    error = true;
+    nbErr++;
+  }
 
-    // X. Detect tables
-    const table = sheet.match(/<table>/g);
-    if (table) {
-        console.warn("[" + data.reference + "][CHECK] TABLE    : " + table[0].substring(0, 100).replace(/\s\n/g, ' '))
-        error = true;
-        nbErr++;
-    }
+  // X. Detect tables
+  const table = sheet.match(/<table>/g);
+  if (table) {
+    console.warn("[" + data.reference + "][CHECK] TABLE    : " + table[0].substring(0, 100).replace(/\s\n/g, " "));
+    error = true;
+    nbErr++;
+  }
 
+  // 4. html tag in content
+  const htmlTag = sheet.match(/<[a-z]>/g);
+  if (htmlTag) {
+    console.warn("[" + data.reference + "][CHECK] HTML_TAG    : " + htmlTag[0].substring(0, 100).replace(/\s\n/g, " "));
+    error = true;
+    nbErr += htmlTag.length;
+  }
 
+  // 1. first line alone
+  // const firstLineAlone = sheet.match(/^# (.*)\s\n\s\n(<|[a-zA-Z]|-)/gm);
+  // if (firstLineAlone) {
+  //     console.warn("[" + data.reference + "][CHECK] FIRST_LINE_ALONE    : " + firstLineAlone[0].substring(0, 100).replace(/\s\n/g, ' '))
+  //     error = true;
+  // }
 
-    // 4. html tag in content 
-    const htmlTag = sheet.match(/<[a-z]>/g);
-    if (htmlTag) {
-        console.warn("[" + data.reference + "][CHECK] HTML_TAG    : " + htmlTag[0].substring(0, 100).replace(/\s\n/g, ' '))
-        error = true;
-        nbErr += htmlTag.length
-    }
+  // check 2 parts :
+  // - string not begin with # or - and finish without ponctuation
+  // - string begin with lower case
+  const aloneLine = [...sheet.matchAll(/(^(?!#)(?!-)(.*)[a-z]$)|(^[a-z](.*)$)/gm)];
+  if (aloneLine.length) {
+    console.warn("[" + data.reference + "][CHECK] ALONE_LINE    : " + aloneLine.length); //[0].substring(0, 100).replace(/\s\n/g, ' '));
+    error = true;
+    nbErr += aloneLine.length;
+  }
 
-    // 1. first line alone
-    // const firstLineAlone = sheet.match(/^# (.*)\s\n\s\n(<|[a-zA-Z]|-)/gm);
-    // if (firstLineAlone) {
-    //     console.warn("[" + data.reference + "][CHECK] FIRST_LINE_ALONE    : " + firstLineAlone[0].substring(0, 100).replace(/\s\n/g, ' '))
-    //     error = true;
-    // }
-
-    // check 2 parts : 
-    // - string not begin with # or - and finish without ponctuation
-    // - string begin with lower case
-    const aloneLine = [...sheet.matchAll(/(^(?!#)(?!-)(.*)[a-z]$)|(^[a-z](.*)$)/gm)];
-    if (aloneLine.length) {
-        console.warn("[" + data.reference + "][CHECK] ALONE_LINE    : " + aloneLine.length); //[0].substring(0, 100).replace(/\s\n/g, ' '));
-        error = true;
-        nbErr += aloneLine.length
-    }
-
-    const puces = [...sheet.matchAll(/```{=html}\s\n<!-- -->\s\n```/g)];
-    if (puces.length) {
-        console.warn("[" + data.reference + "][CHECK] PUCES    : " + puces.length); //[0].substring(0, 100).replace(/\s\n/g, ' '));
-        error = true;
-        nbErr += puces.length
-    }
-    // /```{=html}\s\n<!-- -->\s\n```/g
-    if (error) { nbErrSheet++; console.log('**') }
-
+  const puces = [...sheet.matchAll(/```{=html}\s\n<!-- -->\s\n```/g)];
+  if (puces.length) {
+    console.warn("[" + data.reference + "][CHECK] PUCES    : " + puces.length); //[0].substring(0, 100).replace(/\s\n/g, ' '));
+    error = true;
+    nbErr += puces.length;
+  }
+  // /```{=html}\s\n<!-- -->\s\n```/g
+  if (error) {
+    nbErrSheet++;
+    console.log("**");
+  }
 }
 
-console.log(`End script ! ${nbErrSheet} sheet(s) with error, with a total of ${nbErr} error(s)`)
+console.log(`End script ! ${nbErrSheet} sheet(s) with error, with a total of ${nbErr} error(s)`);
+*/
